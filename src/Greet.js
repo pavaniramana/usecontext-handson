45496778a2c3f6eb3489685848b6f6ccf05a80b9
pavaniramana <pavanikatasala@gmail.com>
2022-04-27 13:29:50 +0530

    first

--- src/Greet.js
+++ src/Greet.js
@@ -3,11 +3,19 @@ import {UserContext} from './UserContext';
 
 const Greet = () => {
     const[user,setUser] = useContext(UserContext)
+
+    const updatedValue = () => {
+        setUser((prevState)=>({
+            ...prevState,
+            age:25
+        }))
+    }
   return (
     <div>
         
         <h5>this is the explanation of useContext Hook</h5>
         {user.name} || {user.is} || {user.age} || {user.dept} || {user.salary}
+        <button onClick={updatedValue}>click here to change value</button>
     </div>
   )
 }
